1a49299c2f12b140bce23ed41edf8aa1fccaf054
markjfannon <markjfannon@Markbook-Pro.local>
2024-12-01 21:37:55 +0000

    Plagiarism time I guess. Making day 1 a bit better

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,62 +1,39 @@
 use std::{
-    cmp::{max, min},
     collections::HashMap,
     fs,
     iter::zip,
 };
 
 fn main() {
-    let contents = fs::read_to_string("input.txt").expect("Unable to read the file!");
+    let lines = fs::read_to_string("input.txt").expect("Unable to read the file!");
+    let lines = lines.split("\n");
 
-    println!("{}", part1(&contents));
-    println!("{}", part2(&contents));
+    let (list1, list2) : (Vec<i32>, Vec<i32>) = lines.into_iter()
+        .collect::<Vec<_>>()
+        .iter()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(left, right)| (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap()))
+        .unzip();
+
+    println!("{}", part1(&list1, &list2));
+    println!("{}", part2(&list1, &list2));
 }
 
-fn part1(contents: &str) -> i32 {
-    let lines = contents.split("\n");
-
-    let mut list1: Vec<i32> = vec![];
-    let mut list2: Vec<i32> = vec![];
-
-    for item in lines {
-        let nums: Vec<i32> = item
-            .split_whitespace()
-            .map(|num| num.parse().expect("Item should be an integer"))
-            .collect();
-
-        list1.push(nums[0]);
-        list2.push(nums[1]);
-    }
-
-    list1.sort();
-    list2.sort();
-
-    zip(list1, list2).fold(0, |acc, (x, y)| acc + (max(x, y) - min(x, y)))
+fn part1(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
+    zip(list1, list2).map(|(x, y)| (x - y).abs()).sum()
 }
 
-fn part2(contents: &str) -> i32 {
-    let x = contents.split("\n");
-
-    let mut list: Vec<i32> = vec![];
+fn part2(list1: &Vec<i32>, list2: &Vec<i32>) -> i32 {
     let mut map: HashMap<i32, i32> = HashMap::new();
-
-    for item in x {
-        let nums: Vec<i32> = item
-            .split_whitespace()
-            .map(|num| num.parse().expect("Item should be an integer"))
-            .collect();
-
-        list.push(nums[0]);
-
-        map.entry(nums[1])
+    
+    for item in list2 {
+        map.entry(*item)
             .and_modify(|count| *count += 1)
             .or_insert(1);
     }
 
-    let sums = list.iter().fold(0, |acc, x| match map.get(x) {
+    list1.iter().fold(0, |acc, x| match map.get(x) {
         Some(num) => acc + (x * num),
         None => acc,
-    });
-
-    sums
+    })
 }
